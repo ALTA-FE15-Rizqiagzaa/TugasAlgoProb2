9a99b7cbb32cc04b87a7790b822b89a46b2da6a0
rizqiagza <138458784+rizqiagza@users.noreply.github.com>
2023-07-07 13:39:22 +0700

    Part 2 problem 1

---
+++ Part2prob1.ts
@@ -0,0 +1,20 @@
+function studentScore (number :number): string{
+    if(number >=80 && number <= 100){
+        return "Nilai A";
+    } else if (number >= 65 && number <= 79){
+        return "Nilai B";
+    } else if (number >= 50 && number <= 64){
+        return "Nilai B+";
+    } else if (number >= 35 && number <= 49){
+        return "Nilai C";
+    } else if (number >= 0 && number <= 34){
+        return "Nilai D";
+    } else {
+        return "invalid";
+    }
+}
+
+
+
+//inputan
+console.log(studentScore(102))
